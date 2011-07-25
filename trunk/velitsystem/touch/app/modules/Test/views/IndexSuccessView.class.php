b183ddf113f5a1b3f9abe6328931cdfa2caddfd6
tierrarara@gmail.com <tierrarara@gmail.com@604acd35-9507-fc46-4930-bf6f6425ee10>
2011-07-25 22:14:19 +0000

    modulo test para pruebas

---
+++ trunk/velitsystem/touch/app/modules/Test/views/IndexSuccessView.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class Test_IndexSuccessView extends AppTestBaseView
+{
+	public function executeHtml(AgaviRequestDataHolder $rd)
+	{
+		$this->setupHtml($rd);
+		
+		$this->setAttribute('_title', 'Index');
+	}
+}
+
+?>
