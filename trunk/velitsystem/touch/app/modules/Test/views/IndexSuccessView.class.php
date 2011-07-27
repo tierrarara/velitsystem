8b316becb4ac138cd39b41b17e8d55e3e749176d
tierrarara@gmail.com <tierrarara@gmail.com@604acd35-9507-fc46-4930-bf6f6425ee10>
2011-07-27 01:29:39 +0000

    convirtiendo la respuesta en el lugar indicado segun el output_type

--- trunk/velitsystem/touch/app/modules/Test/views/IndexSuccessView.class.php
+++ trunk/velitsystem/touch/app/modules/Test/views/IndexSuccessView.class.php
@@ -12,6 +12,10 @@ class Test_IndexSuccessView extends AppTestBaseView
 	public function executeJson(AgaviRequestDataHolder $rd)
 	{
 		$this->setupHtml($rd);
+		
+		$l = $this->getAttribute('l');
+		
+		$this->setAttribute('l', json_encode($l));
 	}
 }
 
