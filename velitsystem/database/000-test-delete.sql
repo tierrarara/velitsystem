c9ac813b0535e054bcd39f4d8857812e2b5b9471
tierrarara@gmail.com <tierrarara@gmail.com@604acd35-9507-fc46-4930-bf6f6425ee10>
2011-07-31 12:33:52 +0000

    quitar oids en el postgres de otra manera compatible con otras versiones

--- velitsystem/database/000-test-delete.sql
+++ velitsystem/database/000-test-delete.sql
@@ -1,11 +1,9 @@
+SET default_with_oids = false;
+
 CREATE TABLE test_1
 (
    id serial, 
    "name" character varying(16), 
    phone character varying(16), 
     PRIMARY KEY (id)
-) 
-WITH (
-  OIDS = FALSE
-)
-;
+);
