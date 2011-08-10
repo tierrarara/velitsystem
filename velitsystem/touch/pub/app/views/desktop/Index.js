504d826f0c9f796a4e8a8965dde32853da0ce2db
tierrarara@gmail.com <tierrarara@gmail.com@604acd35-9507-fc46-4930-bf6f6425ee10>
2011-08-10 22:49:44 +0000

    inventando con el dashboard

--- velitsystem/touch/pub/app/views/desktop/Index.js
+++ velitsystem/touch/pub/app/views/desktop/Index.js
@@ -8,27 +8,35 @@
  * @version    $Id$
  */
 
-App.views.DesktopIndex = Ext.extend(Ext.Panel, {
+App.views.DesktopIndex = Ext.extend(Ext.TabPanel, {
 
     initComponent: function() {
-        Ext.apply(this, {
-            id: 'DesktopIndex',
-            html: 'bienvenido a touch.velitsystem.com',
-            dockedItems: [{
-            	xtype: 'toolbar',
-            	title: 'titulo del panel "desktopindex"',
-            	items: [{
-            		text: 'Logout',
-            		handler: function (evt) {
-            			Ext.dispatch({
-            				controller: 'Auth',
-            	            action    : 'logOut'
-            			});
-            		}
-            	}]
-            }]
-        });
-        App.views.Viewport.superclass.initComponent.apply(this, arguments);
+    	Ext.apply(this, {
+    		title: 'Tab Panel',
+    		id: 'DesktopIndex',
+    		fullscreen: false,
+    		tabBar: {
+    			dock: 'bottom',
+    			scroll: 'horizontal',
+    			sortable: true,
+    			layout: {
+    				pack: 'center'
+    			}
+    		},
+    		items: [
+                    { iconCls: 'bookmarks', title: 'Bookmarks', html:  ' html Bookmarks'  },
+                    { iconCls: 'download', title: 'Download', html: 'tab Download' },
+                    { iconCls: 'favorites', title: 'Favorites',html: 'tab Favorites' },
+                    { iconCls: 'info', title: 'Info' },
+                    { iconCls: 'more', title: 'More' },
+                    { iconCls: 'search', title: 'Search' },
+                    { iconCls: 'settings', title: 'Settings' },
+                    { iconCls: 'team', title: 'Team' },
+                    { iconCls: 'time', title: 'Time' },
+                    { iconCls: 'user', title: 'User' }
+                ]
+    	})
+    	App.views.DesktopIndex.superclass.initComponent.call(this);
     }
 });
 
