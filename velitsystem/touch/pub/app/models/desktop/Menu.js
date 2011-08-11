322fdab890f824105de83d7941e71d18997c60b6
tierrarara@gmail.com <tierrarara@gmail.com@604acd35-9507-fc46-4930-bf6f6425ee10>
2011-08-11 21:22:14 +0000

    modelo del menu del Desktop - Main Menu

---
+++ velitsystem/touch/pub/app/models/desktop/Menu.js
@@ -0,0 +1,24 @@
+/**
+ * @category Touch Velit
+ * @author Rafael Guerrero <tierrarara@gmail.com>
+ * @package App
+ * @subpackage Model
+ * @copyright Copyright (c) 2011 Velit System (http://www.velitsystem.com)
+ * @license http://www.velitsystem.com/licence
+ * @version $Id$
+ */
+
+Ext.regModel('DesktopMenu', {
+	fields : [ {
+		name : '_dex_row_menu_id',
+		type : 'int'
+	}, 'name', 'description', 'controller', 'action' ],
+	proxy: {
+		type: 'ajax',
+		url : '/Desktop/Menu',
+		reader: {
+			type: 'json',
+			root: 'data'
+		}
+	}
+});
