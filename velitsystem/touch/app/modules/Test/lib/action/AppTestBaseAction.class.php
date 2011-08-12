7a655adc93879d25fd6024756668c76162181f49
tierrarara@gmail.com <tierrarara@gmail.com@604acd35-9507-fc46-4930-bf6f6425ee10>
2011-08-12 21:07:28 +0000

    el modulo de test no necesita autenticacion

--- velitsystem/touch/app/modules/Test/lib/action/AppTestBaseAction.class.php
+++ velitsystem/touch/app/modules/Test/lib/action/AppTestBaseAction.class.php
@@ -3,9 +3,8 @@
 /**
  * The base action from which all Test module actions inherit.
  */
-class AppTestBaseAction extends AppBaseAction
-{
-
+class AppTestBaseAction extends AppBaseAction {
+	public function isSecure() {
+		return false;
+	}
 }
-
-?>
