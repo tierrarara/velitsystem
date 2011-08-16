2ec58999cb0f2d56bf9257a9f3fa8440c7d11f7d
tierrarara@gmail.com <tierrarara@gmail.com@604acd35-9507-fc46-4930-bf6f6425ee10>
2011-08-16 13:30:10 +0000

    Customer Module add

---
+++ trunk/velitsystem/touch/app/modules/Customer/actions/ListAction.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class Customer_ListAction extends AppCustomerBaseAction {
+
+	
+	public function validateRead(AgaviRequestDataHolder $rd) {
+		// los parametros se pueden validar tambien en el routing
+	}
+	
+	public function executeRead(AgaviRequestDataHolder $rd) {
+		
+	}
+	
+	public function handleReadError(AgaviRequestDataHolder $rd) {
+		
+	}
+	
+}
