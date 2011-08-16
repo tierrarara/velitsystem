8c9391e5cbae8f7b897e06d93d61c2b9b310ec40
tierrarara@gmail.com <tierrarara@gmail.com@604acd35-9507-fc46-4930-bf6f6425ee10>
2011-08-16 13:39:23 +0000

    seleccionado el dia automaticamente, cambiando apariencia de botones por grupo de botones tipo opcion, nuevo camo de búsqueda

--- trunk/velitsystem/touch/pub/app/views/customer/Attention.js
+++ trunk/velitsystem/touch/pub/app/views/customer/Attention.js
@@ -5,127 +5,137 @@
  * @subpackage View
  * @copyright  Copyright (c) 2011 Velit System (http://www.velitsystem.com)
  * @license    http://www.velitsystem.com/licence
- * @version    $Id$
+ * @version    $Id: Index.js 107 2011-08-11 22:18:30Z tierrarara@gmail.com $
  */
 
 
 App.views.CustomerAttention = Ext.extend(Ext.Panel, {
 	
-	layout: 'fit',
-	
 	initComponent: function(){
 
-	// Objeto para cargar la data definida en la vista de Customer
-	var Store = new Ext.data.Store({
-		model:'Customer'
-	});
-	
-	Ext.apply(this, {
-		fullscreen:true,
-		layout:'card',
-		
-		items:[{
-			xtype:'list',
-			store: Store,
+		// TODO: deberia existir un objeto global Date
+		var date = new Date();
 			
-			grouped: false,
-			itemTpl:'<div class="thumb-wrap">{name} - <small>{address1}</small></div>',
-			onItemDisclosure: true
-		}],
+		// Objeto para cargar la data definida en la vista de Customer
+		var Store = new Ext.data.Store({
+			model:'Customer'
+		});
 		
-		
-		
-		dockedItems: [{
-			xtype:'toolbar',
-			dock:'top',
+		Ext.apply(this, {
+			id: 'CustomerAttention',
+			layout: 'fit',
+			
+			// lista de clientens
 			items:[{
-				xtype:'button',
-				text:'Search By Name',
-				// TODO: Hasta que no se haga la funcion que realizara este
-				// boton se deja en Empty
-				handler:Ext.emptyFn
-			}]
+				xtype:'list',
+				store: Store,
 				
-		},
-		{
-			xtype:'toolbar',
-			dock:'bottom',
-			items:[{
-				xtype:'button',
-				text:'Monday',
-				keyDay:0,
-				// TODO: Hasta que no se haga la funcion que realizara este
-				// boton se deja en Empty
-				handler:this.onSelectDay
+				grouped: false,
+				itemTpl:'<div class="thumb-wrap">{name} - <small>{address1}</small></div>',
+				onItemDisclosure: true
+			}],
+			
+			// botones
+			dockedItems: [{
+				xtype:'toolbar',
+				dock:'top',
+				items:[{
+					xtype:'button',
+					text:'Search By Name',
+					// TODO: Hasta que no se haga la funcion que realizara este
+					// boton se deja en Empty
+					handler:Ext.emptyFn
+				}]
+					
 			},
 			{
-				xtype:'button',
-				text:'Tuesday',
-				keyDay:1,
-				// TODO: Hasta que no se haga la funcion que realizara este
-				// boton se deja en Empty
-				handler:this.onSelectDay
-			},
-			{
-				xtype:'button',
-				text:'Wednsday',
-				keyDay:2,
-				// TODO: Hasta que no se haga la funcion que realizara este
-				// boton se deja en Empty
-				handler:this.onSelectDay
-			},
-			{
-				xtype:'button',
-				text:'Thursday',
-				keyDay:3,
-				// TODO: Hasta que no se haga la funcion que realizara este
-				// boton se deja en Empty
-				handler:this.onSelectDay
-			},
-			{
-				xtype:'button',
-				text:'Friday',
-				keyDay:4,
-				// TODO: Hasta que no se haga la funcion que realizara este
-				// boton se deja en Empty
-				handler:this.onSelectDay
-			},
-			{
-				xtype:'button',
-				text:'Saturday',
-				keyDay:5,
-				// TODO: Hasta que no se haga la funcion que realizara este
-				// boton se deja en Empty
-				handler:this.onSelectDay
-			},
-			{
-				xtype:'button',
-				text:'Sunday',
-				keyDay:6,
-				// TODO: Hasta que no se haga la funcion que realizara este
-				// boton se deja en Empty
-				handler:this.onSelectDay
+				xtype:'toolbar',
+				dock:'bottom',
+				items:[{
+					xtype: 'segmentedbutton',
+					items:[{
+						xtype:'button',
+						text:'Monday',
+						pressed: 1 == date.getDay(),
+						keyDay:1,
+						handler:this.onSelectDay
+					},
+					{
+						xtype:'button',
+						text:'Tuesday',
+						pressed: 2 == date.getDay(),
+						keyDay:2,
+						handler:this.onSelectDay
+					},
+					{
+						xtype:'button',
+						text:'Wednsday',
+						pressed: 3 == date.getDay(),
+						keyDay:3,
+						handler:this.onSelectDay
+					},
+					{
+						xtype:'button',
+						text:'Thursday',
+						pressed: 4 == date.getDay(),
+						keyDay:4,
+						handler:this.onSelectDay
+					},
+					{
+						xtype:'button',
+						text:'Friday',
+						pressed: 5 == date.getDay(),
+						keyDay:5,
+						handler:this.onSelectDay
+					},
+					{
+						xtype:'button',
+						text:'Saturday',
+						pressed: 6 == date.getDay(),
+						keyDay:6,
+						handler:this.onSelectDay
+					},
+					{
+						xtype:'button',
+						text:'Sunday',
+						pressed: 0 == date.getDay(),
+						keyDay:0,
+						handler:this.onSelectDay
+					}]
+				},{
+					xtype: 'searchfield',
+					name: 'search',
+					placeHolder: 'Customer Search By Name',
+					listeners: {
+						action: function () {
+							console.debug('action');
+						}
+					}
+				}]
+			}],
+			
+			listeners: {
+				render: function () {
+					console.debug('render');
+				}
 			}
-			]
-		}]
-	}
+		});
 	
-	);
-	
-	
-	
-	
-	App.views.CustomerAttention.superclass.initComponent.call(this);
-},
+		App.views.CustomerAttention.superclass.initComponent.call(this);
+	},
 
 	onSelectDay: function(event){
-	
-	
+		
 		Ext.dispatch({
 			controller:'Customer',
-			action:'selectDay',
-			day:event.keyDay
+			action:'SelectDay',
+			day:event.keyDay,
+			historyUrl: 'Customer/SelectDay/'+event.keyDay
 		});
-}
+	},
+	
+	
 
 });
+
+Ext.reg('App.views.CustomerAttention', App.views.CustomerAttention );
