98373544597ce63bc8f7208f5b504bb4925cf79b
tierrarara@gmail.com <tierrarara@gmail.com@604acd35-9507-fc46-4930-bf6f6425ee10>
2011-08-19 16:13:25 +0000

    reiniciando archivos para que el svn reconozca el tad id

---
+++ velitsystem/touch/pub/app/views/customer/Attention.js
@@ -0,0 +1,9 @@
+/**
+ * @category   Touch Velit
+ * @author     Rafael Guerrero <tierrarara@gmail.com>
+ * @package    App
+ * @subpackage View
+ * @copyright  Copyright (c) 2011 Velit System (http://www.velitsystem.com)
+ * @license    http://www.velitsystem.com/licence
+ * @version    $Id$
+ */
