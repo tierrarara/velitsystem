dfa67df5bb0f23c17639aca4b7866082085b250c
tierrarara@gmail.com <tierrarara@gmail.com@604acd35-9507-fc46-4930-bf6f6425ee10>
2011-08-29 00:42:34 +0000

    Agregando repositorios y modelos

---
+++ trunk/velitsystem/common/Velit/Model/Warehouse/Header.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * 
+ * @category   Velit
+ * @author     Rafael Guerrero <tierrarara@gmail.com>
+ * @package    Model
+ * @copyright  Copyright (c) 2011 Velit System (http://www.velitsystem.com)
+ * @license    http://www.velitsystem.com/licence
+ * @version    $Id$
+ */
+
+class Velit_Model_Warehouse_Header extends Zend_Db_Table_Row {
+
+}
---
+++ trunk/velitsystem/common/Velit/Repository/Sop/Header.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * 
+ * @category   Velit
+ * @author     Rafael Guerrero <tierrarara@gmail.com>
+ * @package    Repository
+ * @copyright  Copyright (c) 2011 Velit System (http://www.velitsystem.com)
+ * @license    http://www.velitsystem.com/licence
+ * @version    $Id$
+ */
+
+class Velit_Repository_Sop_Header extends Zend_Db_Table {
+
+	protected $_name = 'sop_header';
+	
+	protected $_rowClass = 'Velit_Model_Sop_Header';
+}
